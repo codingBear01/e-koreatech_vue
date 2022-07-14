0c61bb9f2b4f5d30e8276cf5cb49e74e3d80ee36
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-14 13:07:48 +0900

    lec 7. Component 기본

---
+++ vue-test/src/ch7/test1.js
@@ -0,0 +1,12 @@
+const myComponent = {
+  data() {
+    return {
+      name: 'Kang Myeongmo',
+    };
+  },
+  template: /*html*/ `
+  <h3>Component Test {{ name }}</h3>
+  `,
+};
+
+export default myComponent;
